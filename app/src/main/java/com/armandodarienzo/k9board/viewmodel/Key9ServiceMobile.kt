4cde0f2dee72a0f104e6dfab0f6237124d15ff6d
armyj <armando.darienzo94@gmail.com>
2024-05-15 15:04:00 +0200

    Change to the service to force the recomposition in the onStartInputView

--- app/src/main/java/com/armandodarienzo/k9board/viewmodel/Key9ServiceMobile.kt
+++ app/src/main/java/com/armandodarienzo/k9board/viewmodel/Key9ServiceMobile.kt
@@ -3,6 +3,7 @@ package com.armandodarienzo.k9board.viewmodel
 import android.content.res.Configuration
 import android.os.Build
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.lifecycle.ViewTreeViewModelStoreOwner
@@ -10,19 +11,21 @@ import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 
 import com.armandodarienzo.k9board.shared.Key9Service
 import com.armandodarienzo.k9board.ui.keyboard.ComposeKeyboardView
+import dagger.hilt.android.AndroidEntryPoint
 
-class Key9ServiceMobile: Key9Service() {
+@AndroidEntryPoint
+class Key9ServiceMobile(): Key9Service() {
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreateInputView(): View {
 
         //background color needs to be changed here, otherwise system won't pick up correctly
-        var backgroundColorId = android.R.color.system_accent1_50
+        //var backgroundColorId = android.R.color.system_accent1_50
 
         val nightModeFlags: Int = this.resources.configuration.uiMode and
                 Configuration.UI_MODE_NIGHT_MASK
 
-        backgroundColorId = when (nightModeFlags) {
+        var backgroundColorId = when (nightModeFlags) {
             Configuration.UI_MODE_NIGHT_YES -> android.R.color.system_neutral1_900
             else -> android.R.color.system_neutral2_50
         }
@@ -45,6 +48,16 @@ class Key9ServiceMobile: Key9Service() {
         }
 
         return super.onCreateInputView()
+    }
+
+    override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
+
+        /* This is needed because otherwise recomposition wont be triggered
+        *  when user preferences are changed. Won't be needed anymore when
+        * and if AbstractComposeView can work with HiltViewModel */
+        (view as ComposeKeyboardView).disposeComposition()
+
+        super.onStartInputView(info, restarting)
     }
 
 }
