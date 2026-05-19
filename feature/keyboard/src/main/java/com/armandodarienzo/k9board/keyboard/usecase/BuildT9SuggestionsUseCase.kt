14e84a2d801482dc50d0d3a21289649b289da345
armandodarienzo <armando.darienzo94@gmail.com>
2026-05-19 23:32:17 +0200

    Updated `BuildT9SuggestionsUseCase` and `UpsertTypedWordsUseCase` to run on `Dispatchers.IO` and changed `KeyboardViewModel` scope to use `Dispatchers.Default`.

--- feature/keyboard/src/main/java/com/armandodarienzo/k9board/keyboard/usecase/BuildT9SuggestionsUseCase.kt
+++ feature/keyboard/src/main/java/com/armandodarienzo/k9board/keyboard/usecase/BuildT9SuggestionsUseCase.kt
@@ -2,6 +2,8 @@ package com.armandodarienzo.k9board.keyboard.usecase
 
 import com.armandodarienzo.k9board.model.Word
 import com.armandodarienzo.k9board.repository.WordRepositoryProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class BuildT9SuggestionsUseCase @Inject constructor(
@@ -9,16 +11,17 @@ class BuildT9SuggestionsUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(t9code: String, languageTag: String, maxLength: Int): List<Word> {
         if (t9code.isEmpty() || languageTag.isEmpty()) return emptyList()
-        val repo = wordRepositoryProvider.getForLanguage(languageTag)
-        val words = repo.getWordsByCode(t9code).toMutableList()
-
-        if (words.isEmpty() && t9code.length > 2) {
-            var attempt = 1
-            while (words.isEmpty() && t9code.length + attempt <= maxLength) {
-                words.addAll(repo.getPlaceholderWordsByCode(t9code, attempt))
-                attempt++
+        return withContext(Dispatchers.IO) {
+            val repo = wordRepositoryProvider.getForLanguage(languageTag)
+            val words = repo.getWordsByCode(t9code).toMutableList()
+            if (words.isEmpty() && t9code.length > 2) {
+                var attempt = 1
+                while (words.isEmpty() && t9code.length + attempt <= maxLength) {
+                    words.addAll(repo.getPlaceholderWordsByCode(t9code, attempt))
+                    attempt++
+                }
             }
+            words
         }
-        return words
     }
 }
